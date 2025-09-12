11153ba2ef44d4a43be4f37d8c565464202af885
Gloria <gloriafish027@gmail.com>
2025-09-12 14:35:22 +0800

    新增swiper

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log('Hello world');
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const photoSwiper = new Swiper('.photo-swiper', {
+  modules: [Navigation],
+  slidesPerView: 2.5, // 顯示 2.5 張
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  breakpoints: {
+    0: { slidesPerView: 1.2 },
+    768: { slidesPerView: 2 },
+    1200: { slidesPerView: 2.5 },
+  },
+});
+
+
